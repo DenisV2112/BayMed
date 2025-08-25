b0ea2af40ea70ad0d0c9b2a29e7425dfad4ab3aa
Denis Sanchez <deslvvpr@gmail.com>
2025-08-25 15:10:09 -0500

    commit/v1

---
+++ Frontend/src/pages/calculator.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return `
+    <section>
+      <h1>Bienvenido a BAYMED</h1>
+      <p>Esta es la página de inicio.</p>
+    </section>
+  `;
+}
+
+export const title = "Inicio";
