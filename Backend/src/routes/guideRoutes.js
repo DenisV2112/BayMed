6f5c85956fc5eb4b873b83b55c0accaeb43c2aa3
Denis Sanchez <86267586+DenisV2112@users.noreply.github.com>
2025-08-31 19:26:51 -0500

    Delete Backend/src directory

--- Backend/src/routes/guideRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getAllGuides, getGuidesBySpecialty } = require('../controllers/guideController');
-
-const router = express.Router();
-
-router.get('/', getAllGuides);
-router.get('/:specialty', getGuidesBySpecialty);
-
-module.exports = router;
