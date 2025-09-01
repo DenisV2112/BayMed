14b792045f0152507543814b7bb31d5a49f62a8c
Denis Sanchez <deslvvpr@gmail.com>
2025-08-31 19:45:44 -0500

    fix: updating nodemodules and package-lock.json

---
+++ backend/src/models/Guide.js
@@ -0,0 +1,12 @@
+// src/models/Guides.js
+const mongoose = require('mongoose');
+
+const GuideSchema = new mongoose.Schema({
+  specialty: { type: String, required: true }, // cardiología, neurología...
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  content: { type: String, required: true }, // texto largo
+  image: { type: String, required: false }   // nombre del archivo (ej: heart.png)
+}, { timestamps: true });
+
+module.exports = mongoose.model('Guide', GuideSchema);
